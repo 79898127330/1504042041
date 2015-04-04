3ba109e721c63703a07a3a1875d8c039177e5fde
79898127330 <79898127330@BK.RU>
2015-04-05 02:07:30 +0300

    dokuwiki template

--- config.php
+++ config.php
@@ -1,13 +1,17 @@
-<?php /* Configuration file for LionWiki. */
-$WIKI_TITLE = 'My new wiki'; // name of the site
+<?php
 
-// SHA1 hash of password. If empty (or commented out), no password is required
-// $PASSWORD = sha1("my_password");
+	// Configuration file
 
-$TEMPLATE = 'templates/dandelion.html'; // presentation template
+	$WIKI_TITLE = '';// name of the site
 
-// if true, you need to fill password for reading pages too
-// before setting to true, read http://lionwiki.0o.cz/index.php?page=UserGuide%3A+How+to+use+PROTECTED_READ
-$PROTECTED_READ = false;
+	// SHA1 hash of password. If empty (or commented out), no password is required
+	$PASSWORD=sha1('');
 
-$NO_HTML = true; // XSS protection
+	$TEMPLATE='templates/dandelion.html';// presentation template
+	$TEMPLATE='templates/dokuwiki/index.html';// presentation template
+
+	// if true, you need to fill password for reading pages too
+	// before setting to true, read http://lionwiki.0o.cz/index.php?page=UserGuide%3A+How+to+use+PROTECTED_READ
+	$PROTECTED_READ=false;
+
+	$NO_HTML=true;// XSS protection
