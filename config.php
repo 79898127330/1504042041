78b8bf4a208c03ca2f1624f1617b55404547bdb7
79898127330 <79898127330@BK.RU>
2015-04-05 09:14:55 +0300

    +

--- config.php
+++ config.php
@@ -10,8 +10,8 @@
 	$TEMPLATE='templates/dandelion.html';// presentation template
 	$TEMPLATE='templates/dokuwiki/index.html';// presentation template
 
-	// if true, you need to fill password for reading pages too
-	// before setting to true, read http://lionwiki.0o.cz/index.php?page=UserGuide%3A+How+to+use+PROTECTED_READ
+	// if true, you need to fill password for reading pages too before setting to true,
+	// read http://lionwiki.0o.cz/index.php?page=UserGuide%3A+How+to+use+PROTECTED_READ
 	$PROTECTED_READ=false;
 
-	$NO_HTML=true;// XSS protection
+	$NO_HTML=false;// XSS protection
